022f844720f36c1631c0c1f95fd9e5ff3e523e47
Samuel Lippert <lippertz@gmail.com>
2017-07-06 16:41:03 -0500

    Remove extra tree import

--- iFactr.UI/Views/Items/CollectionItems/ICustomItem.cs
+++
@@ -1,7 +0,0 @@
-namespace iFactr.Core.Layers
-{
-    /// <summary>
-    /// Defines a custom <see cref="iFactr.Core.Layers.iItem"/> or <see cref="iFactr.Core.Forms.Field"/> class.
-    /// </summary>
-    public interface ICustomItem { }
-}
--- iFactr.UI/Views/Layers/IDetailLayer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace iFactr.Core.Layers
-{
-    /// <summary>
-    /// Defines a layer that is displayed in the detail pane of a large form-factor device.
-    /// </summary>
-    [Obsolete("Use the PreferredPane attribute instead.")]
-    public interface IDetailLayer { }
-}
