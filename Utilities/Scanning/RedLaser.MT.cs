4da06d311353b7938b5308ca1d8185eb8f6029a8
Ben Horgen <ben@benhorgen.com>
2018-05-07 13:36:18 -0500

    Squashed 'MonoCross/' content from commit a1bcbde
    git-subtree-dir: MonoCross
    git-subtree-split: a1bcbdea6913df828d11395a8303df91888807be

---
+++ Utilities/Scanning/RedLaser.MT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonoCross.Utilities.Barcode
+{
+    public partial class RedLaser
+    {
+        public static RedLaser GetInstance()
+        {
+            if (_instance == null)
+                _instance = new RedLaser();
+            return _instance;
+        }
+    }
+}
